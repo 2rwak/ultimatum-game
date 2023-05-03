19bf9c98114df174b7714d5a397367cc62842d35
Arwa <62561456+2rwak@users.noreply.github.com>
2023-05-03 16:42:29 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+import Ultimatum from './Ultimatum';
+
+// import Ultimatum1 from './Ultimatum1';
+
+function App() {
+  return (
+    <div className="App">
+      <Ultimatum/>
+      {/* <Ultimatum1/> */}
+    </div>
+  );
+}
+
+export default App;
